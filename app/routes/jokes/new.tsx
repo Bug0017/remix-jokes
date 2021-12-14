538e59d84bfb46da18ce2def4415c8646f618a52
Surendar <bug0017@outlook.com>
2021-12-14 12:08:11 +0530

    mutation is done

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,31 +1,58 @@
+import type { ActionFunction } from "remix";
+import { redirect } from "remix";
 import { apply, tw } from "twind";
+import { db } from "~/utils/db.serve";
 
-export default function NewJokeRoute(){
-    return (
-      <form action="post">
-        <div className={tw`${inputContainer}`}>
-          <label className={tw`${inputLabel}`} htmlFor="name">
-            Name:
-          </label>
-          <input
-            className={tw`${inputField}`}
-            type="text"
-            name="name"
-            id="name"
-          />
-        </div>
-        <div className={tw`${inputContainer}`}>
-          <label className={tw`${inputLabel}`} htmlFor="content">
-            Content:
-          </label>
-          <textarea className={tw`${inputField}`} name="content" id="content" />
-        </div>
-        <button type="submit" className={tw`p-2 border border-gray-300`}>Add</button>
-      </form>
-    );
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error(`Form not submitted correctly.`);
+  }
+
+
+  const fields = { name, content };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
+export default function NewJokeRoute() {
+  return (
+    <form method="post">
+      <div className={tw`${inputContainer}`}>
+        <label className={tw`${inputLabel}`} htmlFor="name">
+          Name:
+        </label>
+        <input
+          className={tw`${inputField}`}
+          type="text"
+          name="name"
+          id="name"
+        />
+      </div>
+      <div className={tw`${inputContainer}`}>
+        <label className={tw`${inputLabel}`} htmlFor="content">
+          Content:
+        </label>
+        <textarea
+          className={tw`${inputField}`}
+          name="content"
+          id="content"
+          typeof="text"
+        />
+      </div>
+      <button type="submit" className={tw`p-2 border border-gray-300`}>
+        Add
+      </button>
+    </form>
+  );
 }
 
-
 const inputContainer = apply`flex flex-col p-3`;
-const inputLabel = apply`text-xs text-gray-400 font-semibold`
-const inputField = apply`p-1 border border-gray-500 w-64`
+const inputLabel = apply`text-xs text-gray-400 font-semibold`;
+const inputField = apply`p-1 border  text-gray-900 border-gray-500 w-64`;
